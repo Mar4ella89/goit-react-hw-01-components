9e5b3bf47347c1cb8883019f3dafcd9e25ac9b06
Marat Baikin <3371335@gmail.com>
2023-01-17 00:15:05 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,6 @@
 import css from './Profile.module.css';
+import PropTypes from "prop-types";
+
 
 export const Profile = ({ username, tag, location, stats, avatar }) => {
   return (
@@ -32,3 +34,12 @@ export const Profile = ({ username, tag, location, stats, avatar }) => {
     </div>
   );
 };
+
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+}
